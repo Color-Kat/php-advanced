fc428fa797d2b32bbf60455631082a12423d3807
Color-Kat <katik7979@gmail.com>
2021-09-15 21:03:44 +0300

    for loop, prime numbers algorithm

--- app/loops.php
+++ app/loops.php
@@ -1,6 +1,29 @@
 <?php
 
-echo '========== loops.php ========= <br><br>';
+echo '<br><br> ========== loops.php ========= <br><br>';
 echo '---------------- LOOPS ----------------- <br>';
+
+echo 'cycle FOR: <br>';
+for ($i=0, $j=11, $i<10; $i++, --$j;) {
+    echo "$i - $j <br>";
+}
+
+echo '<br>cycle FOR - prime numbers: <br>';
+
+for ($i=2; $i<100; $i++) {
+    $flag = false;
+    for ($j=2; $j<$i; $j++) {
+        if ( ($i % $j) != 0 ) {
+            continue;
+        } else {
+            $flag = true;
+            break;
+        }
+    }
+
+    if (!$flag) echo "$i ";
+    $flag = false;
+}
+
 
 echo '<br> ==========================';
