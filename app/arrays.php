cb98ad981dd142e8c51f826502531bc58cec596b
Color-Kat <katik7979@gmail.com>
2021-09-16 20:16:14 +0300

    in_array, array_search, array comparison

--- app/arrays.php
+++ app/arrays.php
@@ -33,4 +33,37 @@ echo "before: x = $x, y = $y <br>";
 list($x, $y) = [$y, $x];
 echo "after: x = $x, y = $y <br>";
 
+echo '---------- <br>';
+echo 'merge arrays using $arr1 + $arr2: <br>';
+$arr1 = [0, 1, 2,42];
+// no indexes intersect
+$arr2 = [2 => 41, 4 => 43];
+print_r ($arr2 + $arr1 );
+
+echo '<br>---------- <br>';
+echo 'comparison of arrays: <br>';
+
+$arr1 = [0, 1, 2,42];
+// no indexes intersect
+$arr2 = ['0', true, 2, '42'];
+print_r($arr1);
+print_r($arr2);
+echo '<br>';
+if ($arr1 == $arr2) echo 'arrays are equal<br>';
+else echo 'arrays are not equal<br>';
+
+echo '---------- <br>';
+echo 'in array (0, $arr2): <br>';
+echo in_array(0, $arr2) ? 'true' : 'false';
+echo '<br>';
+
+echo 'in_array strict (0, $arr2, true): <br>';
+echo in_array(0, $arr2, true) ? 'true' : 'false';
+echo '<br>';
+
+echo 'array_search (0, $arr2): <br>';
+echo array_search(0, $arr2);
+echo '<br>';
+
+
 echo '<br> ==========================';
