abaa8dcd697a57962b6436c160fe167d39bb4199
Color-Kat <katik7979@gmail.com>
2021-09-29 21:10:49 +0300

    psr-1

--- app/psr-0/index.php
+++ app/psr-0/index.php
@@ -4,6 +4,10 @@ require('autoload.php');
 echo '<br><br> ===== psr-0/index.php ===== <br><br>';
 echo '----------------- psr-0 ------------------ <br>';
 
+// there must be the autoloader
+// namespaces StudlyCase\StudlyCase.php
+// or less preferably - Vendor_Prefix_Class.php
+
 $http_client = new ColorKat\Http\Client();
 
 echo '<br> ==========================';
---
+++ app/psr-1/index.php
@@ -0,0 +1,6 @@
+<?php
+
+// camelCase - function and methods
+// UPPER_CASE - const
+// StudlyCase - classes
+// avoid to declaration and side effect in one file
