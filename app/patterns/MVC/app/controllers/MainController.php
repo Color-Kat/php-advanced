5ca4f17c1cd033928be67ac93d5debe0b49ae539
Color-Kat <katik7979@gmail.com>
2021-10-15 21:05:21 +0300

    db connetction

--- app/patterns/MVC/app/controllers/MainController.php
+++ app/patterns/MVC/app/controllers/MainController.php
@@ -3,12 +3,15 @@
 namespace app\controllers;
 
 use \app\core\Controller;
+use \app\lib\Db;
 
 class MainController extends Controller
 {
     public function indexAction()
     {
         $testVar = 'it is Module View Controller!';
+
+        $db = new Db();
 
         $this->view->render('Main page title', compact('testVar'));
     }
--- app/patterns/MVC/app/lib/Db.php
+++ app/patterns/MVC/app/lib/Db.php
@@ -4,7 +4,21 @@ namespace app\lib;
 
 class Db
 {
-    public function __construct() {
-        echo __METHOD__;
+    protected $db;
+
+    public function __construct()
+    {
+        $config = require APP . '/config/db.php';
+
+        extract($config);
+
+        $dns = $config['driver'] .
+            ':host=' . $config['host'] .
+            ((!empty($config['port'])) ? (';port=' . $config['port']) : '') .
+            ';dbname=' . $config['dbname'];
+
+        echo $dns;
+
+        $this->db = new \PDO($dns, $config['user'], $config['password']);
     }
 }
